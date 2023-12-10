faff6b8bc42fd0e15ad5c1d68137819520d21d2e
my-rice <dvdrsi2017@gmail.com>
2023-12-10 16:13:22 +0100

    ,,,

--- plot_derivates.py
+++ plot_derivates.py
@@ -1,16 +1,5 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from matplotlib.patches import Rectangle
-
-import matplotlib.pyplot as plt
-
-def my_cilinder(x,u):
-    r = 0.4
-    k = 1000
-    height = 100
-    
-    temp = height*(-1/(1+np.exp(-k*((x[0]-u[0])**2+(x[1]-u[1])**2-(r**2)))) + 1)
-    return temp
 
 
 bound1 = -1.5
@@ -20,62 +9,115 @@ bound4 = 1.0
 
 resolution = 0.1
 
+obs_points = np.array(np.mat('0 0 0 0 0;0.2 0.4 0.6 0.8 -0.8;0 0 0 0 0'))
+goal_points = np.array(np.mat('-1.4; -0.8; 0'))
+    
 def V(x,y):
     return x**2+y**2
 
+def my_cilinder(x,u):
+    r = 0.4
+    k = 1000
+    height = 100
+    
+    temp = height*(-1/(1+np.exp(-k*((x[0]-u[0])**2+(x[1]-u[1])**2-(r**2)))) + 1)
+    return temp
 
-def V2(x,y):
-    a = 0.5**2
-    b = 0.5**2
-    return sympy.sqrt((x**2/(a)) + (y**2/(b)))
+def my_logpdf(x, u, covar):
+    k = len(x)  # dimension
+    #print("u.shape\n",u.shape,"x.shape\n",x.shape)
+    #print("u\n",u)
+    #print("x\n",x)
+    a = np.transpose(x - u)
+    b = np.linalg.inv(covar)
+    c = x - u
+    d = np.matmul(a, b)
+    e = np.matmul(d, c)
+    numer = np.exp(-0.5 * e)
+    f = (2 * np.pi)**k
+    g = np.linalg.det(covar)
+    denom = np.sqrt(f * g)
+    pdf = numer / denom
+    return pdf
 
 
-def cost(x,y):
-    value = 0.05
-    obs_points = np.array(np.mat('0 0 0 0 0;0.2 0.4 0.6 0.8 -0.8;0 0 0 0 0'))
-    goal_points = np.array(np.mat('-1.4; -0.8; 0'))
+def state_cost(x,y):
     state = np.array([x,y])
+
+    v = np.array([0.02, 0.02], dtype=np.float32)
+    covar = np.diag(v)
     gauss_sum = 0
 
-    for i in range(np.size(obs_points,axis=1)):        
-        gauss_sum += my_cilinder(state[:2],obs_points[:2,i])
+    for i in range(np.size(obs_points,axis=1)):
+        gauss_sum += 20*my_logpdf(state[:2],obs_points[:2,i],covar)
+
+    cost = 30*((state[0]-goal_points[0])**2 + (state[1]-goal_points[1])**2) + gauss_sum + 10*(np.exp(-0.5*((state[0]-(-1.5))/0.02)**2)/(0.02*np.sqrt(2*np.pi))
+                + np.exp(-0.5*((state[0]-1.5)/0.02)**2)/(0.02*np.sqrt(2*np.pi)) + np.exp(-0.5*((state[1]-1.0)/0.02)**2)/(0.02*np.sqrt(2*np.pi))
+                + np.exp(-0.5*((state[1]-(-1.0))/0.02)**2)/(0.02*np.sqrt(2*np.pi)))
+    return(cost)
+
+def state_cost_with_additional_term(x,y):
+    # print("\nx\n",x)
+    # print("\ny\n",y)
+    state = np.array([x,y])
+
+    v = np.array([0.02, 0.02], dtype=np.float32)
+    covar = np.diag(v)
+    gauss_sum = 0
+
+    for i in range(np.size(obs_points,axis=1)):
+        gauss_sum += 20*my_logpdf(state[:2],obs_points[:2,i],covar)
+
+
+    state_cost = ((state[0]-goal_points[0])**2 + (state[1]-goal_points[1])**2) -1/((state[0]-goal_points[0])**2 + (state[1]-goal_points[1])**2 +0.1)
+    cost = 30*state_cost + gauss_sum + 10*(np.exp(-0.5*((state[0]-(-1.5))/0.02)**2)/(0.02*np.sqrt(2*np.pi))
+                + np.exp(-0.5*((state[0]-1.5)/0.02)**2)/(0.02*np.sqrt(2*np.pi)) + np.exp(-0.5*((state[1]-1.0)/0.02)**2)/(0.02*np.sqrt(2*np.pi))
+                + np.exp(-0.5*((state[1]-(-1.0))/0.02)**2)/(0.02*np.sqrt(2*np.pi)))
+    return(cost)
+
+
+def plot_cost(cost_name='V'):
+    x_axis = np.linspace(bound1,bound2,100)
+    y_axis = np.linspace(bound3,bound4,100)
+    X,Y=np.meshgrid(x_axis,y_axis)
+    # print(X.shape)
+    # print("X\n",X)
+    # print(Y.shape)
+    if cost_name == 'state_cost_with_additional_term':
+        Z = np.array([[state_cost_with_additional_term(x,y) for x in x_axis] for y in y_axis] )
+        #costs = np.array([[state_cost((x,y),goal_points,obs_points) for x in X_axis] for y in Y_axis] )
+    elif cost_name == 'state_cost':
+        Z = np.array([[state_cost(x,y) for x in x_axis] for y in y_axis] )
+    else:
+        Z = V(X,Y)
+    #print(Z.shape)
+    Z = np.squeeze(Z)
+    #print("Z\n",Z)
+
+    grad_x, grad_y = np.gradient(Z, x_axis, y_axis)
+
+    fig = plt.figure(figsize=(13,10))
+    ax = fig.add_subplot()
+
+    gradient_module = np.sqrt(grad_x**2 + grad_y**2)
+
+    pcolormesh = ax.pcolormesh(X, Y, gradient_module, cmap='coolwarm', zorder=1, vmax=300)
+
+    # Plot the level curves of the cost function
+    contour= ax.contour(X, Y, gradient_module, cmap='coolwarm', linestyles='dashed', zorder=2, alpha=1, vmax=300) #vmin and vmax are used to make the contour lines more visible
+
+    # Add a colorbar
+    fig.colorbar(pcolormesh,ax=ax) 
+
+    plt.savefig(cost_name+".png")
+    plt.close()
+
+
+if __name__ == "__main__":
+    selected_cost = 'state_cost'
+    plot_cost(selected_cost)
+    print('Done')
+
+
+
     
-    goal_point_cost = (state[0]-goal_points[0])**2 + (state[1]-goal_points[1])**2 # + abs((state[0]-goal_points[0])) + abs((state[1]-goal_points[1]))
-    
-    cost_value = 30*goal_point_cost + gauss_sum + 10*(np.exp(-0.5*((state[0]-(-1.5))/value)**2)/(value*np.exp(2*np.pi))
-                + np.exp(-0.5*((state[0]-1.5)/value)**2)/(value*np.exp(2*np.pi)) + np.exp(-0.5*((state[1]-1.0)/value)**2)/(value*np.exp(2*np.pi))
-                + np.exp(-0.5*((state[1]-(-1.0))/value)**2)/(value*np.exp(2*np.pi)))
-    return cost_value
-
-# x,y=sympy.symbols('x y')
-# fun=cost(x,y)
-# gradfun=[sympy.diff(fun,var) for var in (x,y)]
-# numgradfun=sympy.lambdify([x,y],gradfun)
-
-x = np.linspace(bound1,bound2,500)
-y = np.linspace(bound3,bound4,500)
-X,Y=np.meshgrid(x,y)
-
-#graddat=numgradfun(X,Y)
-
-Z = cost(X,Y)
-grad_x, grad_y = np.gradient(Z, x, y)
-
-fig = plt.figure(figsize=(13,10))
-ax = fig.add_subplot()
-
-Z = np.sqrt(grad_x**2 + grad_y**2)
-
-pcolormesh = ax.pcolormesh(X, Y, Z, cmap='coolwarm', zorder=1, vmax=300)
-
-# Plot the level curves of the cost function
-contour= ax.contour(X, Y, Z, cmap='coolwarm', linestyles='dashed', zorder=2, alpha=1, vmax=300) #vmin and vmax are used to make the contour lines more visible
-
-# Add a colorbar
-fig.colorbar(pcolormesh,ax=ax) 
-
-
-# plt.pcolor(X, Y, Z,vmax=400)
-# plt.colorbar()
-plt.savefig("prova1.png")
-plt.close()
